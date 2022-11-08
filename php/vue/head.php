90c32f6ead2957acd08b64954e56fe555c6a7941
Freddelan <happyrush@hotmail.com>
2022-11-08 10:37:44 +0100

    collaboration

--- php/vue/head.php
+++ php/vue/head.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type="text/css" href="/php/style/stylehead.css">
     <link rel="stylesheet" type="text/css" href="/php/style/contact.css">
     <link rel="stylesheet" type="text/css" href="/php/style/index.css">
-    <link rel="stylesheet" type="text/css" href="/php/style/article_entier.css">
+    <!-- <link rel="stylesheet" type="text/css" href="/php/style/article_entier.css"> -->
     <link rel="stylesheet" type="text/css" href="/php/style/footer.css">
     <title>D.C.L LALLAING</title>
 </head>
@@ -19,7 +19,6 @@
 <body>
     
     <header>
-        
         
         <div class="logoPrin">
              <img class="imglogo" src="/php/Media/logoDCL.png" alt="logo club de foot Lallaing"></img>
@@ -44,7 +43,7 @@
                             <a href="#">U14-U15(13-14 ans)</a></li>
                             <a href="#">Seniors</a></li>
                         </div>
-                     </button>
+                    </button>
                 </div>
 
                 <div class="dropdown">
@@ -58,7 +57,7 @@
                             <a href="#">U14-U15(13-14 ans)</a></li>
                             <a href="#">Seniors</a></li>
                         </div> -->
-                     </button>
+                    </button>
                 </div>
 
                 <div class="dropdown">
@@ -75,70 +74,15 @@
                      </button>
                 
            
-                    <div id="contact"><button class="dropdown-btn" ><a class="contact" href="/php/controler/controler-contact.php">Contact</a></button></div>
-                    <!-- <button class="dropdown-btn"><a href="#">Les entraineurs</a></button> -->
-                        <!-- <div class="dropdown"> -->
-                        <!-- <button class="dropdown-btn"><a href="#">Réseaux sociaux -->
-                            <!-- <div class="content"> -->
-                                <!-- <a  target="_blanck" href="https://www.facebook.com/profile.php?id=100057582574036">Facebook</a></li>
-                                <a target="_blank" href="https://www.instagram.com/dclallaing_off/?hl=fr">Instagram</a></li> -->
-                            <!-- </div> -->
+                    <div id="contact">
+                        <button class="dropdown-btn" ><a class="contact" href="/php/controler/controler-contact.php">Contact</a></button></div>
                         </button>
                     
                         <!-- <button class="dropdown-btn"><a href="/php/controler/controler-contact.php">Etre partenaire de l'equipe</a></button> -->
-</div>
+                </div>
             </nav>
-                </section>
+        </section>
 
-                
-            
-       
-            
-          <!-- <img class="ballon2" src="media/ballon2.png" alt="image de ballon">-->
-            <!-- <nav class="container">   <!--<img class="ballon1" src="media/ballon1.png" alt="image de ballon">-->
-                <!-- <img class="ballon3" src="/ClubFootLallaing/php/media/ballon1.png" alt="image de ballon">
-                    <ul class="section-menu">
-                        <li class="accueil">
-                            <a href="/ClubFootLallaing/php/index.php">Accueil</a>
-                        </li>
-                        <li class="deroulant">
-                            <a href="#">Equipes</a>
-                                <ul class="sous">
-                                    <li><a href="#">U6-U7(5-6 ans)</a></li>
-                                    <li><a href="#">U8-U9(7-8 ans)</a></li>
-                                    <li><a href="#">U10(9 ans)</a></li class="equipe">
-                                    <li><a href="#">U11(10 ans)</a></li class="equipe">
-                                    <li><a href="#">U12-U13(11-12 ans)</a></li class="equipe">
-                                    <li><a href="#">U14-U15(13-14 ans)</a></li class="equipe">
-                                    <li><a href="#">Seniors</a></li>
-                                </ul>
-                        </li>
-                        <li class="deroulant">
-                            <a href="#">Photos</a>
-                                <ul class="sous">
-                                    <li><a href="#">Photos U6-U7</a></li class="photos">
-                                    <li><a href="#">Photos U8-U9</a></li class="photos">
-                                    <li><a href="#">Photos U10</a></li class="photos">
-                                    <li><a href="#">Photos U11</a></li class="photos">
-                                    <li><a href="#">Photos U12-U13</a></li class="photos">
-                                    <li><a href="#">Photos U14-U15</a></li class="photos">
-                                    <li><a href="#">Photos Seniors</a></li class="photos">
-                                </ul>
-                        </li>
-                        <li class="deroulant">
-                            <a href="#">Calendrier</a>
-                                <ul class="sous">  
-                                    <li><a href="#">Calendrier U6-U7</a></li>
-                                    <li><a href="#">Calendrier U8-U9</a></li class="calendrier">
-                                    <li><a href="#">Calendrier U10</a></li class="calendrier">
-                                    <li><a href="#">Calendrier U11</a></li class="calendrier">
-                                    <li><a href="#">Calendrier U12-U13</a></li class="calendrier">
-                                    <li><a href="#">Calendrier U14-U15</a></li class="calendrier">
-                                    <li><a href="#">Calendrier Seniors</a></li>
-                                </ul>
-                        </li>
-                        <li><a href="/ClubFootLallaing/php/controler/controler-contact.php">Contact</a></li>
-                    </ul> -->
         </nav>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
